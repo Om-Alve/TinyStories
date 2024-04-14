0a032ba663c6ee7e838810fd000e1e2500494557
Om-Alve <omalve8174@gmail.com>
2024-04-14 23:57:21 +0530

    initial commit

---
+++ train.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import lightning as L
+import torch.nn.functional as F
+import numpy as np
+from torch.utils.data import Dataset,DataLoader
+import torchmetrics
+import random
+import os
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from utils import GPT2
+
+if __name__ == '__main__':
+    dataset = load_dataset("roneneldan/TinyStories")
+
+
+    tokenizer = AutoTokenizer.from_pretrained('roneneldan/TinyStories')
+    tokenizer.pad_token = tokenizer.eos_token
+
+    def transform(example):
+        return tokenizer(text=example['text'],truncation=True,padding=True,max_length=512,return_tensors='pt')
+
+    ds = dataset.with_transform(transform)
+
+    batch_size = 32
+    train_loader = DataLoader(ds['train'],batch_size=batch_size,num_workers=4,shuffle=True)
+    val_loader = DataLoader(ds['validation'],batch_size=batch_size,num_workers=4)
+
+    vocab_size = tokenizer.vocab_size
+    block_size = 512
+    lr = 3e-4
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    n_embed = 600
+    n_heads = 12
+    n_layer = 4
+    dropout = 0.2
+    num_epochs = 5
+    model = GPT2(n_embed=n_embed,block_size=block_size,vocab_size=vocab_size,n_heads=n_heads,n_layers=n_layer,lr=lr,t_max=num_epochs*len(train_loader))
+
+    callback = L.pytorch.callbacks.ModelCheckpoint(save_top_k=1,mode='max',monitor='validation_loss',save_last=True)
+    trainer = L.Trainer(
+        accelerator='gpu',
+        devices=-1,
+        strategy='ddp',
+        max_epochs=num_epochs,
+        callbacks = [callback],
+        precision='16',
+    )
+
+    trainer.fit(
+            model=model,
+            train_dataloaders=train_loader,
+            val_dataloaders=val_loader,
+        )
---
+++ utils.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+import lightning as L
+import torch.nn.functional as F
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import torchmetrics
+import random
+import os
+
+# os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+class SwiGLU(nn.Module):
+    def __init__(self, n_embed):
+        super().__init__()
+        hidden_size = int(4 * (2/3))
+        self.w = nn.Linear(n_embed, n_embed * hidden_size, bias=False)
+        self.v = nn.Linear(n_embed, n_embed * hidden_size, bias=False)
+        self.w2 = nn.Linear(n_embed * hidden_size, n_embed, bias=False)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        out = self.dropout(self.w2(F.silu(self.w(x)) * self.v(x)))
+        return out
+
+class Head(nn.Module):
+    def __init__(self, head_size, n_embed, block_size, dropout):
+        super().__init__()
+        self.key = nn.Linear(n_embed, head_size, bias=False)
+        self.query = nn.Linear(n_embed, head_size, bias=False)
+        self.value = nn.Linear(n_embed, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # x : (B,T,n_embed)
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        w = q @ k.transpose(-2, -1) / (C ** 0.5)  # (B,T,T)
+        w = w.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        w = F.softmax(w, dim=-1)
+        w = self.dropout(w)
+        v = self.value(x)
+        out = w @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size, n_embed, block_size, dropout):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size, n_embed, block_size, dropout) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embed, n_embed)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, X):
+        out = torch.cat([head(X) for head in self.heads], dim=-1)
+        out = self.proj(out)
+        return self.dropout(out)
+
+class Block(nn.Module):
+    def __init__(self, n_embed, n_heads, block_size, dropout):
+        super().__init__()
+        self.sa_heads = MultiHeadAttention(n_heads, n_embed // n_heads, n_embed, block_size, dropout)
+        self.ffwd = SwiGLU(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+
+    def forward(self, x):
+        x = self.ln1(x)
+        x = x + self.sa_heads(x)
+        x = self.ln2(x)
+        x = x + self.ffwd(x)
+        return x
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, n_embed, n_heads, n_layers, vocab_size, block_size, dropout=0.2):
+        super().__init__()
+        self.embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.positon_embedding = nn.Embedding(block_size, n_embed)
+        self.blocks = nn.Sequential(*[Block(n_embed, n_heads, block_size, dropout) for _ in range(n_layers)])
+        self.ln_f = nn.LayerNorm(n_embed)
+        self.lm_head = nn.Linear(n_embed, vocab_size)
+
+    def forward(self, x):
+        # x : (B,T)
+        B, T = x.shape
+        tok_emb = self.embedding_table(x)  # B,T,n_embed
+        pos_emb = self.positon_embedding(torch.arange(T, device=x.device))  # T,n_embed
+        x = tok_emb + pos_emb  # B,T,n_embed
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lm_head(x)  # B,T,vocab_size
+        return logits
+
+class GPT2(L.LightningModule):
+    def __init__(self, n_embed, n_heads, n_layers, vocab_size, block_size, lr, t_max, dropout=0.2):
+        super().__init__()
+        self.model = TransformerDecoder(n_embed, n_heads, n_layers, vocab_size, block_size, dropout)
+        self.lr = lr
+        self.t_max = t_max
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        logits = self(batch['input_ids'])
+        targets = batch['input_ids'][..., 1:].contiguous()
+        logits = logits[:, :-1, :].contiguous()
+        loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), targets.view(-1))
+        self.log('training_loss', loss, prog_bar=True, on_step=True, on_epoch=False, sync_dist=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        logits = self(batch['input_ids'])
+        targets = batch['input_ids'][..., 1:].contiguous()
+        logits = logits[:, :-1, :].contiguous()
+        loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), targets.view(-1))
+        self.log('validation_loss', loss, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
+
+    def configure_optimizers(self):
+        opt = torch.optim.AdamW(self.parameters(), lr=self.lr)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=opt, T_max=self.t_max)
+        return {
+            'optimizer': opt,
+            'lr_scheduler': {
+                "scheduler": scheduler,
+                "monitor": "training_loss",
+                "interval": "step",
+                "frequency": 1,
+            }
+        }
