4d748a2a6ed47e20e76fa332b8d88454d0825a14
Om-Alve <omalve8174@gmail.com>
2024-04-15 11:16:15 +0530

    Added cli support

--- train.py
+++ train.py
@@ -10,12 +10,27 @@ import os
 from datasets import load_dataset
 from transformers import AutoTokenizer
 from utils import GPT2
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train GPT2 model")
+    parser.add_argument("--dataset_name", type=str, default="roneneldan/TinyStories", help="Name of the dataset to load")
+    parser.add_argument("--model_name", type=str, default="roneneldan/TinyStories", help="Name of the pretrained model to use")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training")
+    parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate")
+    parser.add_argument("--n_embed", type=int, default=300, help="Embedding size")
+    parser.add_argument("--n_heads", type=int, default=12, help="Number of attention heads")
+    parser.add_argument("--n_layers", type=int, default=4, help="Number of layers")
+    parser.add_argument("--dropout", type=float, default=0.2, help="Dropout rate")
+    parser.add_argument("--num_epochs", type=int, default=5, help="Number of epochs")
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    dataset = load_dataset("roneneldan/TinyStories")
+    args = parse_args()
 
+    dataset = load_dataset(args.dataset_name)
 
-    tokenizer = AutoTokenizer.from_pretrained('roneneldan/TinyStories')
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
     tokenizer.pad_token = tokenizer.eos_token
 
     def transform(example):
@@ -23,20 +38,19 @@ if __name__ == '__main__':
 
     ds = dataset.with_transform(transform)
 
-    batch_size = 32
-    train_loader = DataLoader(ds['train'],batch_size=batch_size,num_workers=4,shuffle=True)
-    val_loader = DataLoader(ds['validation'],batch_size=batch_size,num_workers=4)
+    train_loader = DataLoader(ds['train'],batch_size=args.batch_size,num_workers=4,shuffle=True)
+    val_loader = DataLoader(ds['validation'],batch_size=args.batch_size,num_workers=4)
 
     vocab_size = tokenizer.vocab_size
     block_size = 512
-    lr = 3e-4
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    n_embed = 300
-    n_heads = 12
-    n_layer = 4
-    dropout = 0.2
-    num_epochs = 5
-    model = GPT2(n_embed=n_embed,block_size=block_size,vocab_size=vocab_size,n_heads=n_heads,n_layers=n_layer,lr=lr,t_max=num_epochs*len(train_loader))
+    lr = args.lr
+    n_embed = args.n_embed
+    n_heads = args.n_heads
+    n_layers = args.n_layers
+    dropout = args.dropout
+    num_epochs = args.num_epochs
+
+    model = GPT2(n_embed=n_embed,block_size=block_size,vocab_size=vocab_size,n_heads=n_heads,n_layers=n_layers,lr=lr,t_max=num_epochs*len(train_loader))
 
     callback = L.pytorch.callbacks.ModelCheckpoint(save_top_k=1,mode='max',monitor='validation_loss',save_last=True)
     trainer = L.Trainer(
@@ -44,12 +58,12 @@ if __name__ == '__main__':
         devices=-1,
         strategy='ddp',
         max_epochs=num_epochs,
-        callbacks = [callback],
+        callbacks=[callback],
         precision='16-mixed',
     )
 
     trainer.fit(
-            model=model,
-            train_dataloaders=train_loader,
-            val_dataloaders=val_loader,
-        )
+        model=model,
+        train_dataloaders=train_loader,
+        val_dataloaders=val_loader,
+    )
